0d47f489c9f6c47f777c84099b32ee568d32edac
tktk0430 <h.taku.0430.tech@gmail.com>
2019-03-19 15:50:09 +0000

    ch 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'staticpages#home'
-  get 'staticpages/home'
-  get 'staticpages/help'
-  get 'staticpages/about'
-  
+  get '/help', to: 'staticpages#help'
+  get '/about', to: 'staticpages#about'
+  get '/contact', to: 'staticpages#contact'
+  get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/staticpages_controller_test.rb
+++ test/controllers/staticpages_controller_test.rb
@@ -2,21 +2,26 @@ require 'test_helper'
 
 class StaticpagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get staticpages_home_url
-    
+    get root_path
     assert_response :success
     assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
-    get staticpages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
-    get staticpages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 end
