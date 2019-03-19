b34e22217481728d73f811cebcc8dc310700c319
tktk0430 <h.taku.0430.tech@gmail.com>
2019-03-19 05:28:57 +0000

    Finish static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def page_title(page_title)
+    if page_title.present?
+      "#{page_title} | Ruby on Rails Tutorial Sample App"
+    else
+      "Ruby on Rails Tutorial Sample App"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'staticpages#home'
+  get 'staticpages/home'
+  get 'staticpages/help'
+  get 'staticpages/about'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "application#hello"
 end
